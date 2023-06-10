eca1b56e43fd87c2b3b24f8b52515c934d195560
JayChouJay <1020968571@qq.com>
2023-06-10 12:44:46 +0800

    集数处理

--- src/main/java/org/example/StringUtils.java
+++ src/main/java/org/example/StringUtils.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,10 +17,33 @@ public class StringUtils {
         //System.out.println(list);
         return list;
     }
-    public static List<String> formatOne(String str,String match) {
+
+    public static List<String> formatOne(String str, String match) {
         List<String> list = Arrays.asList(str.split(match));
         //验证
         //System.out.println(list);
         return list;
+    }
+
+    /**
+     * 功能描述：
+     * 将 转xabx cde,xabx cde,xabx cde
+     * 转化为：ab cde \n ab cde \n ab cde
+     */
+    public static List<String> subString(String str, String match) {
+        List<String> list = Arrays.asList(str.split(match));
+        List<String> result = new ArrayList<>();
+        for (String s : list) {
+            StringBuffer buffer = new StringBuffer();
+            String[] strings = s.split(" ");
+            strings[0] = strings[0].substring(1);
+            strings[0] = strings[0].substring(0, strings[0].length() - 1);
+
+            buffer.append(strings[0]);
+            buffer.append(" ");
+            buffer.append(strings[1]);
+            result.add(buffer.toString());
+        }
+        return result;
     }
 }
--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -5,15 +5,24 @@ import java.util.List;
 
 public class StringUtilsTest {
     @Test
-    public void formatOne(){
+    public void formatOne() {
         List<String> list = StringUtils.formatOne("Motor system, nervous system, endocrine system, circulatory system, respiratory system, digestive system, urinary system, reproductive system");
         for (String s : list) {
             System.out.println(s);
         }
     }
+
     @Test
-    public void formatOne2(){
-        List<String> list = StringUtils.formatOne("Motor system, nervous system, endocrine system, circulatory system, respiratory system, digestive system, urinary system, reproductive system",", ");
+    public void formatOne2() {
+        List<String> list = StringUtils.formatOne("Motor system, nervous system, endocrine system, circulatory system, respiratory system, digestive system, urinary system, reproductive system", ", ");
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void subString() {
+        List<String> list = StringUtils.subString("第14集 无聊的周末,第15集 派对惊魂夜,第16集 大仲马,第17集 长舌妇,第18集 你是坏人,第19集 情人节前奏曲,第20集 谁动了我的情人节",",");
         for (String s : list) {
             System.out.println(s);
         }
